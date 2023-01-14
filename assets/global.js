01f530fc0a85667c1010ca04505a9ec32240b02d
Arturo Macias <arturomaciasmx@gmail.com>
2023-01-13 22:27:28 -0800

    Update and fix quantity buttons product page

--- assets/global.js
+++ assets/global.js
@@ -507,7 +507,7 @@ class SliderComponent extends HTMLElement {
     this.pageTotal = this.querySelector('.slider-counter--total');
     this.prevButton = this.querySelector('button[name="previous"]');
     this.nextButton = this.querySelector('button[name="next"]');
-	this.productSlider = this.querySelector('.product-slider-box');
+	  this.productSlider = this.querySelector('.product-slider-box');
     if (!this.slider || !this.nextButton) return;
 
     const resizeObserver = new ResizeObserver(entries => this.initPages());
